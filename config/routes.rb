7fdd91a6d9f0451ee7ee1889abecbd2de29d10e7
F3PiX <F3PiXnl@gmail.com>
2018-01-08 15:05:32 +0100

    Bit of finetuning the scaffold; use prefixed css; add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root 'snippets#index'
   resources :snippets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
